8765cf174c6499886f5a0c914828a6f11dbd4bfe
Hayley Swimelar <hswimelar@gitlab.com>
2020-01-04 10:33:38 -0800

    Add parallel WalkFallback function
    Many of the storage drivers depend on a WalkFallback function
    rather than their own implementation of Walk. This function
    will allow us to begin to migrate the, azure, filesystem,
    gcs, inmemory, oss, and swift storage drivers independently to
    parallelized walks.

--- registry/storage/driver/walk.go
+++ registry/storage/driver/walk.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"sort"
+	"sync"
 
 	"github.com/sirupsen/logrus"
 )
@@ -58,4 +59,85 @@ func WalkFallback(ctx context.Context, driver StorageDriver, from string, f Walk
 		}
 	}
 	return nil
+}
+
+// WalkFallbackParallel is similar to WalkFalkback, but processes files and
+// directories in their own goroutines
+func WalkFallbackParallel(ctx context.Context, driver StorageDriver, from string, f WalkFn) error {
+	var retError error
+	errors := make(chan error)
+	quit := make(chan struct{})
+
+	// If we encounter an error from any goroutine called from within doWalk,
+	// return early from any new goroutines and return that error.
+	go func() {
+		for err := range errors {
+			retError = err
+			close(quit)
+		}
+	}()
+
+	// doWalk spawns and manages it's own goroutines, but it also calls
+	// itself recursively. Passing in a WaitGroup allows us to wait for the
+	// entire walk to complete without blocking on each doWalk call.
+	var wg sync.WaitGroup
+
+	doWalkParallel(ctx, driver, &wg, quit, errors, from, f)
+
+	wg.Wait()
+
+	return retError
+}
+
+func doWalkParallel(ctx context.Context, driver StorageDriver, wg *sync.WaitGroup, quit <-chan struct{}, errors chan<- error, from string, f WalkFn) {
+	select {
+	// The walk was canceled, return to stop requests for pages and prevent gorountines from leaking.
+	case <-quit:
+		return
+	default:
+		children, err := driver.List(ctx, from)
+		if err != nil {
+			errors <- err
+			return
+		}
+		for _, child := range children {
+			wg.Add(1)
+			c := child
+			go func() {
+				defer wg.Done()
+				// TODO(stevvooe): Calling driver.Stat for every entry is quite
+				// expensive when running against backends with a slow Stat
+				// implementation, such as s3. This is very likely a serious
+				// performance bottleneck.
+				fileInfo, err := driver.Stat(ctx, c)
+				if err != nil {
+					switch err.(type) {
+					case PathNotFoundError:
+						// repository was removed in between listing and enumeration. Ignore it.
+						logrus.WithField("path", c).Infof("ignoring deleted path")
+						return
+					default:
+						errors <- err
+						return
+					}
+				}
+				err = f(fileInfo)
+				if err == nil && fileInfo.IsDir() {
+					if err := WalkFallback(ctx, driver, c, f); err != nil {
+						errors <- err
+						return
+					}
+				} else if err == ErrSkipDir {
+					// Stop iteration if it's a file, otherwise noop if it's a directory
+					if !fileInfo.IsDir() {
+						return
+					}
+				} else if err != nil {
+					errors <- err
+					return
+				}
+				return
+			}()
+		}
+	}
 }
