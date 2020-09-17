eaa7be60fa52832dffdef8e62e7f51bb3b0a35e0
João Pereira <jpereira@gitlab.com>
2020-09-17 15:06:21 +0000

    Deprecate Docker Schema v1 compatibility

--- registry/handlers/app.go
+++ registry/handlers/app.go
@@ -149,7 +149,9 @@ func NewApp(ctx context.Context, config *configuration.Configuration) *App {
 		}
 	}
 
-	startUploadPurger(app, app.driver, dcontext.GetLogger(app), purgeConfig)
+	log := dcontext.GetLogger(app)
+
+	startUploadPurger(app, app.driver, log, purgeConfig)
 
 	app.driver, err = applyStorageMiddleware(app.driver, config.Middleware["storage"])
 	if err != nil {
@@ -163,6 +165,9 @@ func NewApp(ctx context.Context, config *configuration.Configuration) *App {
 
 	options := registrymiddleware.GetRegistryOptions()
 	if config.Compatibility.Schema1.TrustKey != "" {
+		log.Warn("DEPRECATION WARNING: Docker Schema v1 compatibility is deprecated and will be removed by January " +
+			"22nd, 2021. Please update Docker Engine to 17.12 or later and rebuild and push any v1 images you might " +
+			"still have. See https://gitlab.com/gitlab-org/container-registry/-/issues/213 for more details.")
 		app.trustKey, err = libtrust.LoadKeyFile(config.Compatibility.Schema1.TrustKey)
 		if err != nil {
 			panic(fmt.Sprintf(`could not load schema1 "signingkey" parameter: %v`, err))
@@ -179,6 +184,9 @@ func NewApp(ctx context.Context, config *configuration.Configuration) *App {
 	options = append(options, storage.Schema1SigningKey(app.trustKey))
 
 	if config.Compatibility.Schema1.Enabled {
+		log.Warn("DEPRECATION WARNING: Docker Schema v1 compatibility is deprecated and will be removed by January " +
+			"22nd, 2021. Please update Docker Engine to 17.12 or later and rebuild and push any v1 images you might " +
+			"still have. See https://gitlab.com/gitlab-org/container-registry/-/issues/213 for more details.")
 		options = append(options, storage.EnableSchema1)
 	}
 
