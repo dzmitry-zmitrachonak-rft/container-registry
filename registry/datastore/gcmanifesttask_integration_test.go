ec506c03c4ffa9ad6dec839e35a23266e6b7daf0
João Pereira <jpereira@gitlab.com>
2021-03-12 12:16:12 +0000

    fix tests

--- registry/datastore/gcmanifesttask_integration_test.go
+++ registry/datastore/gcmanifesttask_integration_test.go
@@ -254,7 +254,7 @@ func TestGcManifestTaskStore_Count(t *testing.T) {
 	require.NoError(t, err)
 
 	// see testdata/fixtures/gc_manifest_review_queue.sql
-	require.Equal(t, 3, count)
+	require.Equal(t, 4, count)
 }
 
 func nextGCManifestTask(t *testing.T) (datastore.Transactor, *models.GCManifestTask) {
@@ -300,17 +300,29 @@ func TestGcManifestTaskStore_Next(t *testing.T) {
 	}
 	require.Equal(t, expectedM2, m2)
 
-	// Calling Next again yields nothing and does not block, as the remaining unlocked record has a review_after in
-	// the future.
+	// The 3rd call should yield the unlocked record with the 3rd oldest review_after.
 	tx3, m3 := nextGCManifestTask(t)
 	defer tx3.Rollback()
-	require.Nil(t, m3)
 
-	// unlocking m2 and calling Next returns m2 once again
-	require.NoError(t, tx2.Rollback())
+	expectedM3 := &models.GCManifestTask{
+		RepositoryID: 4,
+		ManifestID:   4,
+		ReviewAfter:  testutil.ParseTimestamp(t, "2020-06-11 09:11:23.655121", local),
+		ReviewCount:  0,
+	}
+	require.Equal(t, expectedM3, m3)
+
+	// Calling Next again yields nothing and does not block, as the remaining unlocked record has a review_after in
+	// the future.
 	tx4, m4 := nextGCManifestTask(t)
 	defer tx4.Rollback()
-	require.Equal(t, expectedM2, m4)
+	require.Nil(t, m4)
+
+	// unlocking m3 and calling Next returns m3 once again
+	require.NoError(t, tx3.Rollback())
+	tx5, m5 := nextGCManifestTask(t)
+	defer tx5.Rollback()
+	require.Equal(t, expectedM3, m5)
 }
 
 func TestGcManifestTaskStore_Next_None(t *testing.T) {
